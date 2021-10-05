95e98ac1f29625391def4a5962a8ace9d8c1b2b7
MariiaAmetina <ametina.ms@gmail.com>
2021-10-05 13:59:22 +0300

    Final version

---
+++ app.js
@@ -0,0 +1,98 @@
+"use strict";
+/*
+GAME RULES:
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
+
+//Selecting elements
+const player0El = document.querySelector(".player-0-panel");
+const player1El = document.querySelector(".player-1-panel");
+const score0El = document.getElementById("score-0");
+const score1El = document.getElementById("score-1");
+const current0El = document.getElementById("current-0");
+const current1El = document.getElementById("current-1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn-new");
+const btnRoll = document.querySelector(".btn-roll");
+const btnHold = document.querySelector(".btn-hold");
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  //Starting conditions
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("winner");
+  player0El.classList.add("active");
+  player1El.classList.remove("winner", "active");
+};
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current-${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("active");
+  player1El.classList.toggle("active");
+};
+
+//Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    //1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //2. Display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    //3. Check for rolled 1
+    if (dice !== 1) {
+      //Add dice to the current score
+      currentScore += dice;
+      document.getElementById(`current-${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //Swich to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    //1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score-${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      //Finish the game
+      playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player-${activePlayer}-panel`)
+        .classList.add("winner");
+      document
+        .querySelector(`.player-${activePlayer}-panel`)
+        .classList.remove("active");
+    } else {
+      //Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
